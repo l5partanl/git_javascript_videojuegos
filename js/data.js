e02dc21613fafef7df10cf2e9d14970ee1ef0724
Gian Caorlin <caorlingiancarlo@gmail.com>
2025-03-02 17:13:01 +0100

    Finalizado sistema de adición y borrado de items. falta guardado de datos en localStorage.

--- js/data.js
+++ js/data.js
@@ -1,19 +1,27 @@
 //creamos un array vacío donde vamos a ir depositando los peronajes a medida que se submitean
 
 const characters = [
-    /* { name: "Mario", game: "Super Mario Bros" },
-    { name: "Luigi", game: "Super Mario Bros" },
-    { name: "Samus", game: "Super Metroid" },
-    { name: "Lara Croft", game: "Tomb Raider" },
-    { name: "Guybrush Threepwood", game: "Monkey Island" } */
+    /*  { name: "Mario", game: "Super Mario Bros" },
+     { name: "Luigi", game: "Super Mario Bros" },
+     { name: "Samus", game: "Super Metroid" },
+     { name: "Lara Croft", game: "Tomb Raider" },
+     { name: "Guybrush Threepwood", game: "Monkey Island" } */
 ]
 
 
 //función para borrar personaje de la lista con .splice() para quitarlo de la lista y remove para quitarlo del html()
 
+const deletedCharacters = [
+
+]
 
 
-
-function erraseCharacter(list, id) {
-
+function erraseCharacter(id) {
+    let index = characters.findIndex(item => item.id === id);
+    if (index !== -1) {
+        let value = characters.splice(index, 1);
+        deletedCharacters.push(value);
+    }
+    console.log(characters);
+    console.log(deletedCharacters);
 }
--- js/main.js
+++ js/main.js
@@ -9,11 +9,16 @@ const sectionList = document.querySelector('.list ul');
 function printChar(item, domElement) {
     const li = document.createElement('li');
     const h3 = document.createElement('h3')
-    h3.textContent = item.name;
+    h3.textContent = item.name.replace(/\b\w/g, char => char.toUpperCase());//super resumido por copilot, separa cada palabra cortando el string en el caracter " ", pone mayuscula la primera letra y las vuelve a juntar.
     const span = document.createElement('span');
-    span.textContent = item.game;
+    span.textContent = item.game.toUpperCase();
     const button = document.createElement('button');
     button.textContent = "ELIMINAR";
+
+    button.addEventListener('click', function () {
+        erraseCharacter(item.id);
+        printAllChar(characters, sectionList)//Funciona pero no ha de ser buena práctica, revisar con el profe.
+    })
 
     li.append(h3, span, button)
     domElement.appendChild(li)
@@ -25,22 +30,40 @@ function printAllChar(list, domElement) {
         domElement.style.display = "block";
         domElement.innerHTML = '<p><strong>Aún no hay perosonajes cargados</strong> Llena el formulario con con los datos del personaje que quieras y lo veras reflejado aquí una vez que lo registres.</p>'
     } else {
+        domElement.style.display = "grid";
         list.forEach(item => printChar(item, domElement));
     }
 }
 
+let contadorId = 1;
 function saveCharacter(event, array) {
     event.preventDefault();
+
+    const nameValue = inputName.value.trim()
+    const gameValue = inputGame.value.trim()
+    if (nameValue === "" || gameValue === "") {
+        alert("Campo vacío, por favor completar los datos");
+        return;
+    }
+
+
+    if (array.some(char => char.name.toLowerCase() === nameValue.toLowerCase())) {
+        alert("El nombre ya está registrado, elige otro.");
+        return;
+    }
+
     const newChar = {
+        id: contadorId,
         name: inputName.value,
         game: inputGame.value,
     }
     array.push(newChar)
     inputName.value = "";
     inputGame.value = "";
-
+    contadorId++;
     printAllChar(array, sectionList);
 }
 
+printAllChar(characters, sectionList);//init, para primera lectura de datos guardados
 btnAdd.addEventListener('click', (event) => saveCharacter(event, characters));
 
